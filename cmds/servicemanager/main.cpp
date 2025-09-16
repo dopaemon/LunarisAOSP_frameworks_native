miyu-xu/frameworks-native

Optimize the priority of ServiceManager(1/2).
During the application startup, we find that the Servicemanager thread is frequently preempted
due to its low priority, resulting in slow application startup and responsiveness.
We hope to optimize these issues by adjusting the priority to RT(98) of the SM
to avoid time-consuming binder in clients to ServiceManager, and improve application startup and response speed.

Change-Id: I58d6baed9a01b38ec804273063ced7a24d12b842
Signed-off-by: zhanghao56 <zhanghao56@xiaomi.corp-partner.google.com>

cmds/servicemanager/main.cpp
@@ -158,6 +158,16 @@ int main(int argc, char** argv) {
 
     LOG(INFO) << "Starting sm instance on " << driver;
 
+    int sched_policy = SCHED_FIFO;
+    int newPriority = sched_get_priority_min(policy);
+
+    struct sched_param param;
+    param.sched_priority = newPriority;
+
+    if (sched_setscheduler(0, sched_policy, &param)) {
+       LOG(ERROR) << "Failed to set ServiceManager priority to SCHED_FIFO";
+    }
+
     sp<ProcessState> ps = ProcessState::initWithDriver(driver);
     ps->setThreadPoolMaxThreadCount(0);
     ps->setCallRestriction(ProcessState::CallRestriction::FATAL_IF_NOT_ONEWAY);

dopaemon/LunarisAOSP_frameworks_native

Bug: 419835707

cmds/servicemanager/main.cpp
@@ -158,6 +158,16 @@ int main(int argc, char** argv) {
 
     LOG(INFO) << "Starting sm instance on " << driver;
 
+    int sched_policy = SCHED_FIFO;
+    int newPriority = sched_get_priority_min(sched_policy);
+
+    struct sched_param param;
+    param.sched_priority = newPriority;
+
+    if (sched_setscheduler(0, sched_policy, &param)) {
+       LOG(ERROR) << "Failed to set ServiceManager priority to SCHED_FIFO";
+    }
+
     sp<ProcessState> ps = ProcessState::initWithDriver(driver);
     ps->setThreadPoolMaxThreadCount(0);
     ps->setCallRestriction(ProcessState::CallRestriction::FATAL_IF_NOT_ONEWAY);